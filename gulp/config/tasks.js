6fd9a2cfaf634627022d8892d4ca6b3326393667
Стас Миронов <stanislavmironov1995@gmail.com>
2023-08-24 00:50:55 +0300

    app

--- gulp/config/tasks.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-	'./gulp/tasks/pug',
-	'./gulp/tasks/sass',
-	'./gulp/tasks/serve',
-	'./gulp/tasks/watch',
-	'./gulp/tasks/svg',
-	'./gulp/tasks/scripts',
-	'./gulp/tasks/tinypng',
-]
--- gulp/tasks/watch.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-	$.gulp.task('watch', function() {
-		$.gulp.watch('src/pug/**/*.pug', $.gulp.series('pug'));
-		$.gulp.watch('src/dev/sass/**/*.scss', $.gulp.series('sass'));
-		$.gulp.watch('src/dev/js/**/*.js', $.gulp.series('scripts'));
-	});
-}
